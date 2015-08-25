c85754b0f5897edf31bc5252aa124296e40c4f02
DURVASULA Arun <arun@libby.cibiv.univie.ac.at>
2015-08-25 12:26:25 +0200

    added make fasta

--- scripts/samtools_make_fasta.sh
+++ scripts/samtools_make_fasta.sh
@@ -10,4 +10,5 @@ SW=${HOME}/software
 REF=/home/CIBIV/andreaf/reference/atAndSlociColl02.fa
 SAMPLE="${1}"
 OUT="${2}"
-samtools mpileup -C50 -uf ${REF} ${SAMPLE} | bcftools call -c - | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > ${HOME}/psmc/data/${OUT}.fq.gz
+iberia=/home/CIBIV/andreaf/canaries/rawData/bam/iberia_closestToCanaries/southern
+samtools mpileup -C50 -uf ${REF} ${iberia}/${SAMPLE} | bcftools call -c - | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > ${HOME}/psmc/data/${OUT}.fq.gz
